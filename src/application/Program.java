78c011decc57a9b0776758e27de0dece4809637f
Geovane dias <geovanelimadias2011@gmail.com>
2024-01-02 16:05:22 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,25 +38,21 @@ public class Program {
 				System.out.print("Check-out date(dd/MM/yyyy): ");
 				checkOut = sdf.parse(sc.next());
 				
-				Date now = new Date();
-				if (checkIn.before(now) || checkOut.before(now)) {
-					System.out.println("Error in reservation: Reservation dates for update must be future dastes");
-				}
-				else if ( !checkOut.after(checkIn)) {
-					
-					System.out.println("Error in reservation: check-out date must be after check-in date");
+				
+				
+				String error = 	 reservation.upadateDates(checkIn, checkOut);
+				if (error != null) {
+					System.out.println("Error in reservation: " + error);
 				}
 				else {
-					reservation.upadateDates(checkIn, checkOut);
 					System.out.println("Reservation: " +  reservation);
+				}
+					
 					
 				}
 				
 				
-			}
 			
-		
-		
 		
 		
 		
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -39,9 +39,21 @@ public class Reservation {
 		return   TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public void upadateDates(Date checkIn, Date checkOut) {
+	public String upadateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return " Reservation dates for update must be future dastes";
+		}
+		if ( !checkOut.after(checkIn)) {
+			
+			return " check-out date must be after check-in date";
+		}
+		
+		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	@Override
